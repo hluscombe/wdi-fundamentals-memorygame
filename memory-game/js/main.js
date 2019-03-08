f86253bd6634c75f55febdfb9f5fb7954a845607
Hugh Luscombe <hughluscombe@MacBook-Pro.local>
2019-03-08 21:38:57 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,51 +1,58 @@
-//console.log("Up and Running!");
-
 var cards = [
 {
 rank: "Queen",
 suit: "hearts",
-cardImage: "images/queen-of-hearts.png",	
+cardImage: "images/queen-of-hearts.png"	
 },
 {
 rank: "Queen",
 suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png",	
+cardImage: "images/queen-of-diamonds.png"	
 },
 {
 rank: "King",
 suit: "hearts",
-cardImage: "images/king-of-hearts.png",	
+cardImage: "images/king-of-hearts.png"	
 },
 {
 rank: "King",
 suit: "diamonds",
-cardImage: "images/king-of-diamonds.png",	
-},
+cardImage: "images/king-of-diamonds.png"	
+}
 ];
 
 var cardsInPlay = []
 
 var checkForMatch = function() { 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
+		alert("You found a match!");
 } else {
-alert("Sorry, try again.");
+		alert("Sorry, try again.");
 };
 }
 
-var flipCard = function(cardId) {
-	if (cardsInPlay.length === 2) {
-	checkForMatch(); 
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute("src", cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 };
-console.log("User flipped" + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (let i = 0; i < cards.length; i++) {
+	    var cardElement = document.createElement('img');
+			cardElement.setAttribute('src', "images/back.png");
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+}
+createBoard();
 
-checkForMatch();
+function reload() {
+	location.reload();
+}
 
 //alert(cardsInPlay[0] == cardsInPlay [1] ? "You found a match!" : "Sorry, try again");
